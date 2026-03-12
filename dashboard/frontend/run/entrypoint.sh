b5622b05ff0b9c0dffd2046f84fe30db30858110
tianxiu2b2t <atianxiua@163.com>
2026-03-12 23:20:35 +0800

    chore: github workflows

--- dashboard/frontend/run/entrypoint.sh
+++ dashboard/frontend/run/entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 : "${BACKEND_URL:=http://localhost:3000}"
 export BACKEND_URL
-envsubst '${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_URL}' < default.conf.template > /etc/nginx/conf.d/default.conf
 exec "$@"
