8f3cfe449e4b47a35c534fdea7f4af13243217a2
tianxiu2b2t <atianxiua@163.com>
2026-03-17 19:56:36 +0800

    refactor: 可使用环境变量传入配置

--- dashboard/backend/src/config.rs
+++ dashboard/backend/src/config.rs
@@ -2,27 +2,76 @@ use std::sync::OnceLock;
 
 use serde::{Deserialize, Serialize};
 use shared::default::{default_dashboard_api_port, default_database_max_connections};
+use tracing::{Level, event};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct MainConfig {
-    #[serde(default = "default_dashboard_api_port")]
+    #[serde(default = "config_dashboard_api_port")]
     pub port: u16,
+    #[serde(default = "config_database_url")]
     pub database: String,
     #[serde(
-        default = "default_database_max_connections",
+        default = "config_max_connections",
         rename = "database_max_connections"
     )]
     pub max_connections: u32,
-    #[serde(rename = "token_expires")]
+    #[serde(rename = "token_expires", default = "config_token_expires")]
     pub token_exp: u64,
+}
+
+
+fn config_token_expires() -> u64 {
+    // from run env
+    let env = std::env::var("TOKEN_EXPIRES");
+    if let Ok(v) = env && let Ok(value) = v.parse::<u64>() {
+        return value;
+    }
+    60 * 60 * 24 * 7
+}
+
+fn config_dashboard_api_port() -> u16 {
+    // from run env
+    let env = std::env::var("DASHBOARD_API_PORT");
+    if let Ok(v) = env && let Ok(value) = v.parse::<u16>() {
+        return value;
+    }
+    default_dashboard_api_port()
+}
+
+fn config_max_connections() -> u32 {
+    // from run env
+    let env = std::env::var("DATABASE_MAX_CONNECTIONS");
+    if let Ok(v) = env && let Ok(value) = v.parse::<u32>() {
+        return value;
+    }
+    default_database_max_connections()
+}
+
+fn config_database_url() -> String {
+    // from run env
+    let env = std::env::var("DATABASE_URL");
+    if let Ok(v) = env {
+        return v;
+    }
+    "".to_string()
 }
 
 pub static CONFIG: OnceLock<MainConfig> = OnceLock::new();
 
 pub fn init_config() -> anyhow::Result<()> {
     // laod from toml
-    let content = std::fs::read_to_string("./config.toml")?;
-    let config = toml::from_str::<MainConfig>(&content)?;
+    let config = match std::fs::read_to_string("config.toml") {
+        Ok(content) => match toml::from_str::<MainConfig>(&content) {
+            Ok(config) => config,
+            Err(_) => {
+                event!(Level::WARN, "config.toml is invalid, use default config");
+                MainConfig::default()
+            }
+        },
+        Err(_) => {
+            return Err(anyhow::anyhow!("config.toml not found"));
+        }
+    };
     CONFIG.set(config).unwrap();
 
     Ok(())
--- gateway/src/config.rs
+++ gateway/src/config.rs
@@ -5,24 +5,54 @@ use shared::default::default_database_max_connections;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct MainConfig {
+    #[serde(default = "config_database_url")]
     pub database: String,
     #[serde(
-        default = "default_database_max_connections",
+        default = "config_max_connections",
         rename = "database_max_connections"
     )]
     pub max_connections: u32,
+}
+
+fn config_max_connections() -> u32 {
+    // from run env
+    let env = std::env::var("DATABASE_MAX_CONNECTIONS");
+    if let Ok(v) = env && let Ok(value) = v.parse::<u32>() {
+        return value;
+    }
+    default_database_max_connections()
+}
+
+fn config_database_url() -> String {
+    // from run env
+    let env = std::env::var("DATABASE_URL");
+    if let Ok(v) = env {
+        return v;
+    }
+    "".to_string()
 }
 
 pub static CONFIG: OnceLock<MainConfig> = OnceLock::new();
 
 pub fn init_config() -> anyhow::Result<()> {
     // laod from toml
-    let content = std::fs::read_to_string("./config.toml")?;
-    let config = toml::from_str::<MainConfig>(&content)?;
+    let config = match std::fs::read_to_string("config.toml") {
+        Ok(content) => match toml::from_str::<MainConfig>(&content) {
+            Ok(config) => config,
+            Err(_) => {
+                event!(Level::WARN, "config.toml is invalid, use default config");
+                MainConfig::default()
+            }
+        },
+        Err(_) => {
+            return Err(anyhow::anyhow!("config.toml not found"));
+        }
+    };
     CONFIG.set(config).unwrap();
 
     Ok(())
 }
+
 
 pub fn get_config() -> &'static MainConfig {
     CONFIG.get().unwrap()
